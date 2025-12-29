bae382e7e2e134a650ce829eeb9ca68317d0678b
coreux-hash <colin.lheureux@gmail.com>
2025-12-29 07:39:09 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "suivi-calorie-v1";
+const CACHE_NAME = "suivi-calorie-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -38,3 +38,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
